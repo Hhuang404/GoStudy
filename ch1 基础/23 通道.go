d32a660f267d050926194482d41fcde119dcae7e
huanghao <hhao2019@163.com>
2020-05-06 21:45:55 +0800

    add 通道，超时处理

--- ch1 基础/23 通道.go
+++ ch1 基础/23 通道.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+	done <- true
+}
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+// 注意 <- 方向
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
 
 func main() {
 	message := make(chan string)
@@ -17,4 +37,36 @@ func main() {
 
 	fmt.Println(<-mss)
 	fmt.Println(<-mss)
+
+	// 通道同步
+	done := make(chan bool, 1)
+	go worker(done)
+	<-done
+
+	// 通道方向
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "pass messages")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
+
+	// 通道选择器
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "two"
+	}()
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
 }
